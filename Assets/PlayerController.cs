857ed86af2bfe963ff3c186324d591b44f371c3d
Austin Wang <austinxwang@gmail.com>
2023-07-01 12:47:54 -0700

    player controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2.5f;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+    private Vector2 mousePosition;
+
+    private Transform mainCameraTransform;
+    private Vector3 cameraOffset;
+    public float smoothSpeed = 0.125f;
+
+    private void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+        cameraOffset = mainCameraTransform.position - transform.position;
+    }
+
+    private void Update()
+    {
+        // Get input for movement
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        // Calculate movement vector
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        // Face the mouse position
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDirection = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+        // Apply movement
+        rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+
+        // Update camera position smoothly
+        Vector3 desiredCameraPosition = transform.position + cameraOffset;
+        Vector3 smoothedCameraPosition = Vector3.Lerp(mainCameraTransform.position, desiredCameraPosition, smoothSpeed);
+        mainCameraTransform.position = smoothedCameraPosition;
+    }
+}
